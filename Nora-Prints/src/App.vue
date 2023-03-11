492e5a1461caffbb534340309c622fe6a4e2fc92
Emanoxxx <71783361+Emanoxxx@users.noreply.github.com>
2023-03-11 17:34:50 -0600

    feat: iniciando proyecto

---
+++ Nora-Prints/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import Navegacion from '@/components/main/Navegacion.vue'
+</script>
+
+<template>
+  <header>
+    <Navegacion></Navegacion>
+    <Contenido></Contenido>
+  </header>
+</template>
+
+<style scoped></style>
